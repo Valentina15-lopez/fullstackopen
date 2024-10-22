b29ebbd555558a0d4bdab10758d30709e8f15ef4
Valentina15-lopez <valentina.lopez@despegar.com>
2024-10-22 18:19:21 -0300

    parte 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,16 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const calculateAverage = () => {
+    const total = good + neutral + bad;
+    if (total === 0) return 0;
+    return (good * 1 + neutral * 0 + bad * -1) / total;
+  };
+  const calculatePositive = () => {
+    const total = good + neutral + bad;
+    if (total === 0) return "0%";
+    return `${((good / total) * 100).toFixed(2)}%`;
+  };
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -15,6 +25,9 @@ const App = () => {
       <p>good:{good}</p>
       <p>neutral:{neutral}</p>
       <p>bad:{bad}</p>
+      <p>all:{good + neutral + bad}</p>
+      <p>average:{calculateAverage()}</p>
+      <p>positive:{calculatePositive()}</p>
     </div>
   );
 };
